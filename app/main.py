644161123d35199a94be582b27da29bd79137382
Mohamed Magdy <ymmagdyfofo@gmail.com>
2025-08-27 16:07:25 +0300

    added cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.controllers.hello_controller import router as hello_router
 from app.controllers.thread_controller import router as thread_router
 from app.controllers.message_controller import router as message_router
-from fastapi import FastAPI
 from app.config.dbconfig import db_create_tables
+
 
 db_create_tables()  # Create database tables at startup
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(hello_router)
 app.include_router(thread_router, prefix="/threads")
